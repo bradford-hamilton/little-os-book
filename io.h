139e7a4966e03b0cc8164fa11c1427ae6a82f7ef
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2023-05-22 11:26:36 -0600

    add an inb (read byte) assembly func

--- io.h
+++ io.h
@@ -1,6 +1,10 @@
 #ifndef IO_H
 #define IO_H
 
+// Sends the given data to the given I/O port. Defined in io.s
 void outb(unsigned short port, unsigned short data);
+
+// Read a byte from an I/O port.
+unsigned char inb(unsigned short port);
 
 #endif // IO_H
